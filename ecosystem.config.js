29f42e99e0dc3051d57167cfc9f9d1f745c03a13
Abdul-liebert <muhammad065932@smk.belajar.id>
2026-09-26 18:40:20 +0700

    edit ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,17 +2,12 @@ module.exports = {
   apps: [
     {
       name: 'smktibazmav3',
-      script: 'node_modules/next/dist/bin/next',
-      args: 'start',
+      script: '.next/standalone/server.js', // 👈 Mengarahkan ke server standalone
       instances: process.platform === 'win32' ? 1 : 'max',
       exec_mode: process.platform === 'win32' ? 'fork' : 'cluster',
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'development',
-        PORT: 3000,
-      },
       env_production: {
         NODE_ENV: 'production',
         PORT: 3000,
